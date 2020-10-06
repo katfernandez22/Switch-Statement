65e9dedd7292572824a06377c67313547e1d9f6b
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2020-10-05 21:20:16 -0400

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,22 @@
+let restaurantItem = "Chicken";
+let price;
+
+switch (restaurantItem) {
+  case "Burger":
+    price = "$10";
+    break;
+  case "Steak":
+    price = "$25";
+    break;
+  case "Salmon":
+    price = "$20";
+    break;
+  case "Shrimp":
+    price = "$20";
+    break;
+  case "Chicken":
+    price = "$15";
+    break;
+  default:
+    price = "not available";
+}
